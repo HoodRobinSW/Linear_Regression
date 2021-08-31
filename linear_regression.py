069e215d266e30329f1e3a6274bfc9ec623b94cb
Alejandro <aleexortega98@gmail.com>
2021-08-31 21:11:52 +0200

    Added some coments and finishing helping Reggie

--- linear_regression.py
+++ linear_regression.py
@@ -13,11 +13,38 @@ def calculate_error(m, b, point):
     distance = abs(y - y_point)
     return distance
 
+#this is a line that looks like y = x, so (3, 3) should lie on it. thus, error should be 0:
+print(calculate_error(1, 0, (3, 3)))
+#the point (3, 4) should be 1 unit away from the line y = x:
+print(calculate_error(1, 0, (3, 4)))
+#the point (3, 3) should be 1 unit away from the line y = x - 1:
+print(calculate_error(1, -1, (3, 3)))
+#the point (3, 3) should be 5 units away from the line y = -x + 1:
+print(calculate_error(-1, 1, (3, 3)))
+
 def calculate_all_error(m, b, points):
     total = 0
     for point in points:
         total += calculate_error(m, b, point)
     return total
+
+#every point in this dataset lies upon y=x, so the total error should be zero:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 0, datapoints))
+
+#every point in this dataset is 1 unit away from y = x + 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 1, datapoints))
+
+#every point in this dataset is 1 unit away from y = x - 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, -1, datapoints))
+
+
+#the points in this dataset are 1, 5, 9, and 3 units away from y = -x + 1, respectively, so total error should be
+# 1 + 5 + 9 + 3 = 18
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(-1, 1, datapoints))
 
 #2.TRY A BUNCH OF SLOPES AND INTERCEPTS!
 
@@ -33,4 +60,9 @@ for m in possible_ms:
             best_m = m
             best_b = b
             smallest_error = calculate_all_error(m, b, datapoints)
-print(best_m, best_b, smallest_error)
+
+#3.WHAT DOES OUR MODEL PREDICT?
+
+print(get_y(best_m, best_b, 6))
+
+print("Our model predicts that the 6cm ball will bounce 3.5m. \nNow, Reggie can use this model to predict the bounce of all kinds \nof sizes of balls he may choose to include in the ball pit!")
